45da5e8635fe5a2a6d6ecca5d0aeb2070017720e
Sercan Kocagoncu <alisk85@gmail.com>
2018-03-14 23:47:31 +0300

    a simple test case

--- src/com/missingartifacts/Main.java
+++ src/com/missingartifacts/Main.java
@@ -1,8 +1,22 @@
 package com.missingartifacts;
 
+import com.missingartifacts.contract.ICache;
+import com.missingartifacts.enumConstants.CachingStrategy;
+import com.missingartifacts.factory.CacheFactory;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ICache cacheInstance = CacheFactory.getInstance(3, CachingStrategy.LRU);
+        cacheInstance.put("1","test cache 1");
+        cacheInstance.put("2","test cache 2");
+        cacheInstance.put("3","test cache 3");
+
+        cacheInstance.printCache();
+
+        cacheInstance.put("4","test cache 4");
+        cacheInstance.put("5","test cache 5");
+        cacheInstance.printCache();
     }
 }
